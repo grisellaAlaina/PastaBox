f5edf8f19e177e7372dc3b3642a90e7b4a8bed64
grisella <serggood@gmail.com>
2024-03-05 10:19:31 +0300

    init

---
+++ src/test/java/org/pasta/PastaApplicationTests.java
@@ -0,0 +1,13 @@
+package org.pasta;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PastaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
